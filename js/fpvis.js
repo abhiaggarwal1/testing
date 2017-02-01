d0fa206dc65a8686e190575560b3e67e1701c9e5
abhiaggarwal1 <abhi2@ualberta.ca>
2017-01-31 18:44:14 -0800

    Update fpvis.js

--- js/fpvis.js
+++ js/fpvis.js
@@ -37,7 +37,8 @@ var tableStrings = {
 	"bleach" 	: "Bleaching (s)",
 	"mature" 	: "Maturation (min)",
 	"lifetime" 	: "Lifetime (ns)",
-	"RefNum"	: "Reference"
+	"RefNum"	: "Reference",
+	"Addgene"	: "Addgene Plasmid #"
 }
 
 //Protein classes for tables
